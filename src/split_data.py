242e5b4fa325587940d2a0e052ca717b7b2f29c6
Monal Kumar <Monalkumar745@gmail.com>
2021-03-19 21:46:39 +0530

    Stage 3 completed

--- src/split_data.py
+++ src/split_data.py
@@ -10,11 +10,12 @@ def split_and_saved_data(config_path):
     config = read_params(config_path)
     test_data_path = config["split_data"]["test_path"]
     train_data_path = config["split_data"]["train_path"]
-    raw_data_path = config["load_data"]["raw_dataset_csv"]
+    raw_data_path = config["data_source"]["s3_source"]
     split_ratio = config["split_data"]["test_size"]
     random_state = config["base"]["random_state"]
 
-    df = pd.read_csv(raw_data_path, sep=",")
+    df = pd.read_csv(raw_data_path, sep=",").replace('"', '', regex=True)
+
     train, test = train_test_split(
         df,
         test_size=split_ratio,
